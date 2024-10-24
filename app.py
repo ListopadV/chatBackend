010cea78020481b34c2a355ee2526485a591d204
LVO <LVO@d.co>
2024-10-24 19:25:32 +0200

    changed url

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.register_blueprint(chat_blueprint, url_prefix='/chats')
 app.register_blueprint(messages_blueprint, url_prefix='/messages')
 app.register_blueprint(bots_blueprint, url_prefix='/bots')
 
-allowedOrigins = ['']
+allowedOrigins = ['https://chat-frontend-vlo.vercel.app']
 CORS(app, resources={r"/*": {
     "origins": allowedOrigins,
     "methods": ['GET', 'POST', 'DELETE', 'PUT'],
